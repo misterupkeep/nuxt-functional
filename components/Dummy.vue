01c6dd8a79338fe3eada9d7814fc1a8f378efed2
Upkeep <upkeep@airmail.cc>
2021-06-01 12:04:30 +0200

    initial commit

---
+++ components/Dummy.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <Logo text="functional component prop">
+    <span>This is inside the &lt;Logo/&gt; default slot</span>
+  </Logo>
+</template>
+
+<script>
+export default {
+  name: "Dummy"
+}
+</script>
+
+<style scoped>
+
+</style>
